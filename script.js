df9ccba48165f47b063c3e8a4f0edcc9e6f7817a
benngarcia <beng4606@gmail.com>
2020-06-08 23:58:53 -0400

    Added Render() Function to Display all Books in myLibrary

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-let myLibrary = [];
+let myLibrary = [
+  new Book("Gone", "Grant", "200", "true"),
+  new Book("Light", "Grant", "200", "true"),
+  new Book("Monster", "Grant", "200", "false")
+];
 
 function Book(title, author, pages, readhuh) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = readhuh;
+  this.readhuh = readhuh;
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+function addBookToLibrary(title, author, pages, readhuh) {
+  myLibrary.push(new Book(title, author, pages, readhuh));
 }
+
+function render() {
+
+  const container = document.querySelector(".booklist")
+  
+  for (let i = 0; i < myLibrary.length; i++) {
+    let newbook = document.createElement('div');
+    newbook.classList.add('book');
+    newbook.textContent = myLibrary[i].title + "\n" +  myLibrary[i].author +
+      "\n" + myLibrary[i].pages + "\n" + myLibrary[i].readhuh;
+      
+    container.appendChild(newbook);
+  }
+
+}
+
+
+render();
